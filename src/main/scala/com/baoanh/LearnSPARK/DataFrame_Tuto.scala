45fd9f653ca61d2a0239f178ed7c3aba97033f4f
nguyenkhacbaoanh <khacbaoanh.nguyen@gmail.com>
2019-05-19 17:11:28 +0200

    DataFrame Join DataFrame

--- src/main/scala/com/baoanh/LearnSPARK/DataFrame_Tuto.scala
+++ src/main/scala/com/baoanh/LearnSPARK/DataFrame_Tuto.scala
@@ -12,7 +12,7 @@ object DataFrame_Tuto extends App with Context {
                     .option("header", false)
                     .option("inferSchema", true) //detected data type from csv
                     .csv(path_file)
-                    .toDF("id","Station_name","latitude","longtitude","available","zone","date")
+                    .toDF("Id","Station_name","Latitude","Longtitude","Terminal","Zone","Date")
     // data frame                
     stationDF.show(10)
     // DataFrame Schema
@@ -29,23 +29,57 @@ object DataFrame_Tuto extends App with Context {
      */
     
     // Select function to choose the column to show
-    stationDF.select("Station_name", "available").show(10)
+    // stationDF.select("Station_name", "available").show(10)
     
     // filter data
-    stationDF.filter("available >= 15").show(5) // condition expression or SQL like: "Station_name LIKE 'S%'"
+    // stationDF.filter("available >= 15").show(5) // condition expression or SQL like: "Station_name LIKE 'S%'"
     
     //data frame query: count()
-    var avai_gte_15: BigInt = stationDF.filter("available >= 15").count()
-    println(s"Number of available greater than 15: ${avai_gte_15}")
+    // var avai_gte_15: BigInt = stationDF.filter("available >= 15").count()
+    // println(s"Number of available greater than 15: ${avai_gte_15}")
     
     // SQL IN Clause
-    stationDF.filter("available in (15, 19)").show(10)
+    // stationDF.filter("available in (15, 19)").show(10)
     
     // GROUP BY: count number of zone
-    stationDF.groupBy("zone").count().show()
+    // stationDF.groupBy("zone").count().show()
     
     // AGGREGATE
-    stationDF.groupBy("zone").agg(("available","sum"),("zone","count")).show()
+    // stationDF.groupBy("zone").agg(("available","sum"),("zone","count")).show()
+    
+    // Trips data
+    // download from https://raw.githubusercontent.com/data-8/materials-x18/master/lec/x18/1/trip.csv
+    val tripsDF = sparkSession
+                .read
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv(path_dir + "/data/trips.csv")
+                .toDF()
+   
+    // head of trips data
+    tripsDF.show(10)      
+    tripsDF.printSchema()
+    /**
+     * root
+       |-- Trip ID: integer (nullable = true)
+       |-- Duration: integer (nullable = true)
+       |-- Start Date: string (nullable = true)
+       |-- Start Station: string (nullable = true)
+       |-- Start Terminal: integer (nullable = true)
+       |-- End Date: string (nullable = true)
+       |-- End Station: string (nullable = true)
+       |-- End Terminal: integer (nullable = true)
+       |-- Bike #: integer (nullable = true)
+       |-- Subscriber Type: string (nullable = true)
+       |-- Zip Code: string (nullable = true)
+     */
+    
+    // join
+    tripsDF.join(stationDF, // DF join with
+                            stationDF("Station_name")===tripsDF("Start Station"), // criteria join
+                            "left_outer") // kind of join
+            .select("Trip ID","Zone","Date","Station_name")
+            .show(10)
     
     //Stop session
     sparkSession.stop()
