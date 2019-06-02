048a985529f9bf8581806ef0e6460c7bd79c39a8
nguyenkhacbaoanh <khacbaoanh.nguyen@gmail.com>
2019-06-02 18:11:34 +0200

    aggregateByKey method for PairRDD

--- src/main/scala/com/baoanh/LearnSPARK/RDDUnemployement.scala
+++ src/main/scala/com/baoanh/LearnSPARK/RDDUnemployement.scala
@@ -3,21 +3,19 @@ case class Area(code: String, text: String)
 case class Series(id: String, area: String, measure: String, title: String)
 case class LaData(id: String, year: Int, period: Int, value: Double)
 object RDDUnemployement extends App with Context {
-  import com.baoanh.LearnSPARK.Context
-  val path_dir: String = System.getProperty("user.dir")
-  def main(args: Array[String]) = {
-    // Area data
-    val area = sparkContext
-                .textFile(path_dir + "/data/la.area")
+  def main(args: Array[String]):Unit = {
+    import com.baoanh.LearnSPARK.Context
+    val path_dir: String = System.getProperty("user.dir")
+      // Area data
+    val area = sc.textFile(path_dir + "/data/la.area")
                 .filter(!_.contains("area_type_code")) // drop the header text
                 .map { line =>
                   val p = line.split("\t").map(_.trim)
                   Area(p(1), p(2))
-                }.cache()
+                }.cache() //cache method or persist method for memory data to the next using
     area.take(10) foreach println
     // Series data
-    val series = sparkContext
-                  .textFile(path_dir + "/data/la.series")
+    val series = sc.textFile(path_dir + "/data/la.series")
                   .filter(!_.contains("area_type_code")) // drop the header text
                   .map{ line =>
                     val p = line.split("\t").map(_.trim)
@@ -25,15 +23,30 @@ object RDDUnemployement extends App with Context {
                   }.cache()
     series.take(10) foreach println
     // LaData 30 Minnesota
-    val laData = sparkContext
-                  .textFile(path_dir + "/data/la.data.30.Minnesota")
+    val laData = sc.textFile(path_dir + "/data/la.data.30.Minnesota")
                   .filter(!_.contains("series_id")) // drop the header text
                   .map{ line =>
                     val p = line.split("\t").map(_.trim)
                     LaData(p(0), p(1).toInt, p(2).drop(1).toInt, p(3).toDouble)
                   }.cache()
     laData.take(10) foreach println
+    /*
+     * join data processing
+     */
+    val rates = laData.filter(_.id.endsWith("03"))
+    // Pair RDD
+    val decadeGroups = rates.map(d => (d.id, d.year/10) -> d.value)
+    decadeGroups.take(5) foreach println
+    // calculate group average
+    val decadeAvg = decadeGroups.aggregateByKey(0.0 -> 0)({ case ((sum, cnt),v) =>
+        (sum + v, cnt + 1)
+      }, {
+      case ((s1, c1),(s2, c2)) => (s1 + s2, c1 + c2)}).map({case (x,y) => (x,y._1/y._2)})
+   decadeAvg.foreach(println)
     
-    sparkContext.stop()
+
+    
+    
+    sc.stop()
   }
 }
