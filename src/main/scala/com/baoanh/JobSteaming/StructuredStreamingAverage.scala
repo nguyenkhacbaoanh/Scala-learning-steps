45352298df24cc284af9eea013c5bafc5d02177d
nguyenkhacbaoanh <khacbaoanh.nguyen@gmail.com>
2020-11-09 21:49:31 +0100

    clean code

--- src/main/scala/com/baoanh/JobSteaming/StructuredStreamingAverage.scala
+++ src/main/scala/com/baoanh/JobSteaming/StructuredStreamingAverage.scala
@@ -1,15 +1,10 @@
 package com.baoanh.JobSteaming
-import org.apache.spark.sql._
 import org.apache.spark.sql.streaming.StreamingQuery
-import org.apache.spark.streaming._
-import com.baoanh.JobSteaming.Person
-import org.apache.spark.sql.Encoders
 import org.apache.spark.sql.types.{StructField, StructType, StringType, LongType}
 
 object StructuredStreamingAverage extends App with Context {
-    val INPUT_DIRECTORY: String = System.getProperty("user.dir") + "/input_files"
+    val INPUT_DIRECTORY: String = "file://" + System.getProperty("user.dir") + "/input_files"
     println(INPUT_DIRECTORY)
-    //val ssc:StreamingContext = new StreamingContext(sc, Seconds(5))
     override def main(args: Array[String]): Unit = {
         //1. Define the in put data schema
         val personSchema = StructType(
@@ -24,7 +19,7 @@ object StructuredStreamingAverage extends App with Context {
         val personStream = sparkSession
             .readStream
             .schema(personSchema)
-            .json("/Volumes/DataSu/dev/Scala-learning-steps/input_files")
+            .json("/Volumes/DataSu/dev/Scala-learning-steps/input_files") //specify path file
         personStream.printSchema()
         //3. Create a temporary table so we can use SQL query
         personStream.createOrReplaceGlobalTempView("people")
